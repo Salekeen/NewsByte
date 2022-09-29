80a7d4028ef8c531ef5ead6c17fc5856311be021
Salekeen <nawazsalekeennayeem@gmail.com>
2022-09-29 15:34:11 +0600

    trying to fix the encoding bug

--- scrapeData.py
+++ scrapeData.py
@@ -2,6 +2,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from scrapeArticle import get_article
+import csv
+from datetime import datetime
 
 
 html = urlopen("https://www.thedailystar.com/")
@@ -14,15 +16,30 @@ for card in card_labels:
     card_body.append(card.parent)
 
 
-# Finding all the NEWS TYPES
-for card in card_labels:
-    print(card.div.a.get_text())
+# # Finding all the NEWS TYPES
+# for card in card_labels:
+#     print(card.div.a.get_text())
 
-# Finding all the headlines
-for card in card_body:
-    print(card.find('div',{'class':'card-headline'}).get_text().strip())
+# # Finding all the headlines
+# for card in card_body:
+#     print(card.find('div',{'class':'card-headline'}).get_text().strip())
 
-# Finding all the urls
-for card in card_body:
-    print(card.find('div',{'class':'card-headline'}).a["href"])
+# # Finding all the urls
+# for card in card_body:
+#     print(card.find('div',{'class':'card-headline'}).a["href"])
+
+
+
+# writing data to a CSV file
+filename = "./Daily Data/{}.csv".format(datetime.now().strftime("%Y_%m_%d"))
+
+with open (filename, 'w') as file:
+    csv_writer = csv.writer(file)
+
+    for card in card_labels:
+        content = card.div.a.get_text()
+        content = bytes(content, 'UTF-8')
+        content = content.decode('UTF-8')
+        csv_writer.writerow(content)
+
 
