572f7f42a25a0ca15eb2698dd1afac8493780d78
Salekeen <nawazsalekeennayeem@gmail.com>
2022-09-29 15:56:40 +0600

    solved encoding problem

--- scrapeData.py
+++ scrapeData.py
@@ -16,17 +16,20 @@ for card in card_labels:
     card_body.append(card.parent)
 
 
-# # Finding all the NEWS TYPES
-# for card in card_labels:
-#     print(card.div.a.get_text())
+# Finding all the NEWS TYPES
+news_type = []
+for card in card_labels:
+    news_type.append(card.div.a.get_text())
 
-# # Finding all the headlines
-# for card in card_body:
-#     print(card.find('div',{'class':'card-headline'}).get_text().strip())
+# Finding all the headlines
+headlines = []
+for card in card_body:
+    headlines.append(card.find('div',{'class':'card-headline'}).get_text().strip())
 
-# # Finding all the urls
-# for card in card_body:
-#     print(card.find('div',{'class':'card-headline'}).a["href"])
+# Finding all the urls
+urls = []
+for card in card_body:
+    urls.append(card.find('div',{'class':'card-headline'}).a["href"])
 
 
 
@@ -35,11 +38,10 @@ filename = "./Daily Data/{}.csv".format(datetime.now().strftime("%Y_%m_%d"))
 
 with open (filename, 'w') as file:
     csv_writer = csv.writer(file)
+    for index in range(len(card_labels)-1):
+        content = []
 
-    for card in card_labels:
-        content = card.div.a.get_text()
-        content = bytes(content, 'UTF-8')
-        content = content.decode('UTF-8')
+        content.append(news_type[index])
+        content.append(headlines[index])
+        content.append(urls[index])
         csv_writer.writerow(content)
-
-
