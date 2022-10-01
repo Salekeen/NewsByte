d9de245b2bd0bf1c9d20ada1d0bca2b5025b66da
Salekeen <nawazsalekeennayeem@gmail.com>
2022-10-01 10:50:51 +0600

    morning commit

--- Get Data/scrapeDataV2.py
+++ Get Data/scrapeDataV2.py
@@ -5,7 +5,7 @@ from scrapeArticle import get_data
 import csv
 from datetime import datetime
 
-# todays news
+# todays all news
 html = urlopen('https://www.thedailystar.net/todays-news')
 bs = BeautifulSoup(html, 'html.parser')
 all_news_urls = set()
@@ -15,14 +15,14 @@ for data in table_data:
 
 
 
-# # Finding all the top news page urls
-# top_news_urls = set()
-# html = urlopen('https://www.thedailystar.net/top-news')
-# bs = BeautifulSoup(html, 'html.parser')
-# for link in bs.findAll('a'):
-#     if 'href' in link.attrs:
-#         if link.attrs['href'] in all_news_urls:
-#             top_news_urls.add(link.attrs['href'])
+# Finding all the top news page urls
+top_news_urls = set()
+html = urlopen('https://www.thedailystar.net/top-news')
+bs = BeautifulSoup(html, 'html.parser')
+for link in bs.findAll('a'):
+    if 'href' in link.attrs:
+        if link.attrs['href'] in all_news_urls:
+            top_news_urls.add(link.attrs['href'])
 
 
 # top_news_urls = list(top_news_urls)
@@ -48,10 +48,10 @@ def write_to_csv(url,filename):
 # writing data to a CSV file
 filename_all_news = "./Get Data/Daily Data/all_news_{}.csv".format(
     datetime.now().strftime("%Y_%m_%d"))
-# # filename_top_news = "./Get Data/Daily Data/top_news_{}.csv".format(
-# #     datetime.now().strftime("%Y_%m_%d"))
+filename_top_news = "./Get Data/Daily Data/top_news_{}.csv".format(
+    datetime.now().strftime("%Y_%m_%d"))
 
 write_to_csv(all_news_urls,filename_all_news)
-# # write_to_csv(top_news_urls,filename_top_news)
+write_to_csv(top_news_urls,filename_top_news)
 
 
