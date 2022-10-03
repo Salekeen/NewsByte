306b697a2a9e3f40ffe897133fb69a1096f677a3
Salekeen <nawazsalekeennayeem@gmail.com>
2022-10-04 01:02:52 +0600

    updated

--- Prototype/summerizer.py
+++ Prototype/summerizer.py
@@ -4,6 +4,7 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.stemmers import Stemmer
+import json
 
 # Fetching the data from CSV
 data = pd.read_csv(
@@ -19,8 +20,12 @@ stemmer = Stemmer("english")
 # Initializing the Summerizer
 summerizer = Summarizer(stemmer)
 
-for article in articles:
+for index, article in enumerate(articles):
     parser = PlaintextParser.from_string(article,Tokenizer("english"))
+    print(headlines[index])
+    print()
+    for sentence in summerizer(parser.document,2):
+        print(sentence)
+    print("**************")
 
-print("This is fine")
     
