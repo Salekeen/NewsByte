14622f58ccb5387bada3d61f0831f7174588c19e
Salekeen <nawazsalekeennayeem@gmail.com>
2022-10-04 12:43:55 +0600

    added data

--- Prototype/summerizer.py
+++ Prototype/summerizer.py
@@ -4,7 +4,8 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.stemmers import Stemmer
-import json
+import csv
+from datetime import datetime
 
 # Fetching the data from CSV
 data = pd.read_csv(
@@ -20,12 +21,34 @@ stemmer = Stemmer("english")
 # Initializing the Summerizer
 summerizer = Summarizer(stemmer)
 
+def write_to_csv(filename, content):
+    
+    with open(filename, 'a') as file:
+        csv_writer = csv.writer(file)
+        # csv_writer.writerow(["Headline", "Summary"])
+        
+        csv_writer.writerow(content)
+
+# writing data to a CSV file
+filename = "./Prototype/Daily Data/top_news_{}.csv".format(
+    datetime.now().strftime("%Y_%m_%d"))
+
 for index, article in enumerate(articles):
+    content = []
+    summary = ""
+    headline = headlines[index]
     parser = PlaintextParser.from_string(article,Tokenizer("english"))
-    print(headlines[index])
-    print()
     for sentence in summerizer(parser.document,2):
-        print(sentence)
-    print("**************")
+        summary += f"{sentence}\n"
+    
+    content.append(headline)
+    content.append(summary)
+    write_to_csv(filename,content)
+    
+    
+
+
+
+
 
     
