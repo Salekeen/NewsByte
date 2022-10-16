c503b8700232da50527f035a65c238291eee91e2
Salekeen <nawazsalekeennayeem@gmail.com>
2022-10-17 03:45:53 +0600

    restructured

---
+++ KPE/load_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pathlib import Path
+
+
+path = Path("../Get Data/Data/all_news")
+
+
+def get_df():
+
+    df = pd.DataFrame()
+
+    for f in path.iterdir():
+        # reading the file in dataframe
+        try:
+            temp_df = pd.read_csv(f, encoding="utf8")
+        except UnicodeDecodeError:
+            temp_df = pd.read_csv(f, encoding="'windows-1252")
+
+        # adding a single file dataframe to the main dataframe
+        df = pd.concat([df, temp_df], axis=0)
+
+    return df
