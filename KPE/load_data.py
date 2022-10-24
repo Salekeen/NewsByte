f352527d6ba52af41817d862b53bd8aeabfda2f9
Salekeen <nawazsalekeennayeem@gmail.com>
2022-10-24 07:37:59 +0600

    KPE now pulls data from database

--- KPE/load_data.py
+++ KPE/load_data.py
@@ -10,22 +10,21 @@ load_dotenv()
 
 # Establishing Setup
 engine = create_engine(
-    f"postgresql+psycopg2://{os.environ['dbUSERNAME']}:{os.environ['dbPASSWORD']}@localhost:5432/ScrapedData"
+    f"postgresql+psycopg2://{os.environ['dbUSERNAME']}:{os.environ['dbPASSWORD']}@localhost:5432/Scratch"
 )
 metadata = MetaData(engine)
-all_articles = Table('all_articles', metadata, autoload=True)
-top_articles = Table('top_articles', metadata, autoload=True)
+articles_table = Table('articles', metadata, autoload=True)
 connection = engine.connect()
 
 
 def get_last_n_days_data(n=7):
-    
+
     todays_date = datetime.today()
     seventh_previous_days_date = (datetime.today() - timedelta(days=n))
-    s = select([all_articles]).where(
+    s = select([articles_table]).where(
         and_(
-            all_articles.c.date_published <= todays_date,
-            all_articles.c.date_published >= seventh_previous_days_date
+            articles_table.c.date_published <= todays_date,
+            articles_table.c.date_published >= seventh_previous_days_date
         )
     )
     rp = connection.execute(s)
